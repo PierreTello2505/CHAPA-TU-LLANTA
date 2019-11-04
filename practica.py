9461b43bc4318debc424814dc72981a8720c640a
Pierre Carhuaricra Tello <pierretello25@gmail.com>
2019-11-04 10:31:45 -0500

    archivo corregido para la App

--- practica.py
+++ practica.py
@@ -1,10 +1,19 @@
 x=int(input("Ingrese el ancho"))
 y=int(input("Ingrese perfil"))
 z=int(input("Ingrese el radio"))
-T=print(x,"/",y,"R",z)
 
-if x==
-
+if x==255 and y==4 and z==19:
+    print("Llanta: Goodyear Eagle F1 Asymmetric 2 ROF")
+    print(x,"/",y,"R",z)
+    s=["Precio $255","ideal para ciudad", "pagina de compra: www.elssfsf.com"]
+    print(s)
+    print("Control preciso y potente Aproveche al máximo su coche con un neumático fabricado para maximizar el rendimiento de alta potencia. Un compuesto de sílice especial ofrece un agarre fiable en carreteras secas y mojadas con una carcasa rígida que aumenta la precisión de la dirección incluso a alta velocidad.")
+elif x==250 and y==3 and z==20:
+    print("Llanta: Goodyear Eagle F1 Asymmetric 2 ROF")
+    print(T)
+    s=["Precio $255","ideal para ciudad", "pagina de compra: www.elssfsf.com"]
+    print(s)
+    print("Control preciso y potente Aproveche al máximo su coche con un neumático fabricado para maximizar el rendimiento de alta potencia. Un compuesto de sílice especial ofrece un agarre fiable en carreteras secas y mojadas con una carcasa rígida que aumenta la precisión de la dirección incluso a alta velocidad.")
 
 
 
