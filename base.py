f2e862f831711c8e7971ea66b446d17fe7f3ee70
Pierre Carhuaricra <Pierretello25@gmail.com>
2019-11-04 07:47:06 -0500

    Opciones de llantas

---
+++ base.py
@@ -0,0 +1,17 @@
+print("Opciones")
+
+print("1. Automovil")
+print("2. Camioneta")
+print("3. Assurance")
+print("4. Eagle")
+print("5. Wrangler")
+print("6. Efficient")
+print("7. Alto desempeño")
+print("8. Llantas Run on Flat")
+
+while True:
+    x=int(input("Ingrese la opcion:"))
+
+    if x>=1 and x<=10:
+        break
+
