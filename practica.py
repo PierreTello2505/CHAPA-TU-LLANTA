a20977eb2974e04ed62be89924e327316179b701
Pierre Carhuaricra Tello <pierretello25@gmail.com>
2019-11-04 09:15:50 -0500

    cambios

--- practica.py
+++ practica.py
@@ -13,8 +13,55 @@ while True:
     x=int(input("Ingrese la opcion:"))
 
     if x==1:
-        print("jejejejeje")
-    
+        print("Elija modelo de llanta:")
+
+        print("1. Goodyear Eagle F1 Asymmetric 2 ROF")
+        print("2. Goodyear Eagle F1 Asymmetric 3")
+        print("3. Goodyear Assurance")
+        print("4. Goodyear EfficientGrip Performance ROF")
+        print("5. Goodyear Eagle F1 Asymmetric 2")
+        print("6. Direction Touring")
+        print("7. Goodyear EfficientGrip ROF")
+        print("8. Goodyear Eagle F1 Asymmetric")
+        print("9. Kelly Edge Sport")
+        print("10. Goodyear Excellence ROF")
+        print("11. Goodyear EfficientGrip Performance")
+        print("12. Kelly Edge Touring")
+        print("13. Goodyear EfficientGrip")
+
+        while True:
+            a=int(input("Escriba opcion:"))
+
+            if a==1:
+                print("dvdd")
+            elif a==2:
+                print("wert")
+            elif a==3:
+                print("fddg")
+            elif a==4:
+                print("yujjk")
+            elif a==5:
+                print("bdhfhd")
+            elif a==6:
+                print("hjkkl")
+            elif a==7:
+                print("fddg")
+            elif a==8:
+                print("klhf")
+            elif a==9:
+                print("bdhfhd")
+            elif a==10:
+                print("jldg")
+            elif a==11:
+                print("fddg")
+            elif a==12:
+                print("gdssd")
+            elif a==13:
+                print("bdhfhd")
+            else:
+                print("Opcion no valida")
+            
+                
     elif x==2:
         print("njsnsdjd")
 
@@ -34,12 +81,11 @@ while True:
         print("kndjbjd")
 
     elif x==8:
-        print("jdgkdnf")
+        print("jdgkhdnf")
 
     else:
         print("Opcion invalida")
-        
-    
+   
     if x<=10 and x>=1:
         break 
 
