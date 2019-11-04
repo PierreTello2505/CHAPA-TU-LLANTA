a7296430cc26f0116054f70313544844a5318989
Alexis Colchado <alex.cdlc10@gmail.com>
2019-11-04 10:39:38 -0500

    version2017comerciales

---
+++ trabajo2.py
@@ -0,0 +1,33 @@
+print("opciones")
+print("1. VEHÍCULO")
+print("2. MEDIDAD DE LLANTAS")
+
+while True:
+    n=int(input("¿Cómo le gustaría hacer la busqueda?"))
+
+    if n==1:
+        a=int(input("Ingrese año del vehículo: "))
+
+        if a==2017:
+            print("1. AICHI SAN")
+            print("2. ALFA ROMEO")
+            print("3. ASTON MARTIN")
+            print("4. AUDI")
+            print("5. AUTOCRAFT")
+            print("6. BAIC")
+            m=int(input("Ingrese la marca de su vehículo: "))
+            if m==1:
+                print("1. HIACE")
+                n=int(input("Elija el modelo de su vehículo: "))
+                if n==1:
+                    print("Tiene estas opciones para su vehiculo:")
+                    print("1. HIACE 1.3 JUPTIER")
+                    print("2. HIACE 2.8 TDI VAN")
+                        
+            
+                    
+
+            
+
+                    
+                    
