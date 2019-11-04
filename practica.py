3bd26c5c09ce5b77650dd5e8c68f9b0e748a4ada
Pierre Carhuaricra Tello <pierretello25@gmail.com>
2019-11-04 10:03:41 -0500

    boceto

--- practica.py
+++ practica.py
@@ -1,95 +1,11 @@
-print("Opciones")
+x=int(input("Ingrese el ancho"))
+y=int(input("Ingrese perfil"))
+z=int(input("Ingrese el radio"))
+T=print(x,"/",y,"R",z)
 
-print("1. Automovil")
-print("2. Camioneta")
-print("3. Assurance")
-print("4. Eagle")
-print("5. Wrangler")
-print("6. Efficient")
-print("7. Alto desempeño")
-print("8. Llantas Run on Flat")
+if x==
 
-while True:
-    x=int(input("Ingrese la opcion:"))
 
-    if x==1:
-        print("Elija modelo de llanta:")
-
-        print("1. Goodyear Eagle F1 Asymmetric 2 ROF")
-        print("2. Goodyear Eagle F1 Asymmetric 3")
-        print("3. Goodyear Assurance")
-        print("4. Goodyear EfficientGrip Performance ROF")
-        print("5. Goodyear Eagle F1 Asymmetric 2")
-        print("6. Direction Touring")
-        print("7. Goodyear EfficientGrip ROF")
-        print("8. Goodyear Eagle F1 Asymmetric")
-        print("9. Kelly Edge Sport")
-        print("10. Goodyear Excellence ROF")
-        print("11. Goodyear EfficientGrip Performance")
-        print("12. Kelly Edge Touring")
-        print("13. Goodyear EfficientGrip")
-
-        while True:
-            a=int(input("Escriba opcion:"))
-
-            if a==1:
-                print("Precio, pagina, clase de terrono, valoracion, comentarios")
-            elif a==2:
-                print("Precio, pagina, clase de terrono, valoracion, comentarios")
-            elif a==3:
-                print("Precio, pagina, clase de terrono, valoracion, comentarios")
-            elif a==4:
-                print("Precio, pagina, clase de terrono, valoracion, comentarios")
-            elif a==5:
-                print("Precio, pagina, clase de terrono, valoracion, comentarios")
-            elif a==6:
-                print("Precio, pagina, clase de terrono, valoracion, comentarios")
-            elif a==7:
-                print("Precio, pagina, clase de terrono, valoracion, comentarios")
-            elif a==8:
-                print("Precio, pagina, clase de terrono, valoracion, comentarios")
-            elif a==9:
-                print("Precio, pagina, clase de terrono, valoracion, comentarios")
-            elif a==10:
-                print("Precio, pagina, clase de terrono, valoracion, comentarios")
-            elif a==11:
-                print("Precio, pagina, clase de terrono, valoracion, comentarios")
-            elif a==12:
-                print("Precio, pagina, clase de terrono, valoracion, comentarios")
-            elif a==13:
-                print("Precio, pagina, clase de terrono, valoracion, comentarios")
-            else:
-                print("Opcion no valida")
-            if a<=13 and x>=1:
-                break 
-            
-                
-    elif x==2:
-        print("njsnsdjd")
-
-    elif x==3:
-        print("ieieiei")
-
-    elif x==4:
-        print("uururr")
-
-    elif x==5:
-        print("icnfjkn")
-
-    elif x==6:
-        print("ndfgjndf")
-    
-    elif x==7:
-        print("kndjbjd")
-
-    elif x==8:
-        print("jdgkhdnf")
-
-    else:
-        print("Opcion invalida")
-   
-    if x<=10 and x>=1:
-        break 
 
 
     
