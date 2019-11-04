268df3af8159d0991044bc7bc7a0781ce711eddb
Pierre Carhuaricra Tello <pierretello25@gmail.com>
2019-11-04 10:03:27 -0500

    se cambiara la base

--- base.py
+++ base.py
@@ -1,6 +1,6 @@
 print("Opciones")
 
-print("1. Automovil")
+print("1. Llantas generales para automoviles Automovil")
 print("2. Camioneta")
 print("3. Assurance")
 print("4. Eagle")
@@ -9,7 +9,7 @@ print("6. Efficient")
 print("7. Alto desempeño")
 print("8. Llantas Run on Flat")
 
-while True:
+while True:                                       #para el print- print("modelo de llanta", x .....)
     x=int(input("Ingrese la opcion:"))
 
     if x==1:
@@ -65,25 +65,319 @@ while True:
             
                 
     elif x==2:
-        print("njsnsdjd")
+        print("Elija modelo de llanta:")
+
+        print("1. Goodyear EfficientGrip Performance SUV")
+        print("2. Goodyear Wrangler All-Terrain Adventure")
+        print("3. Goodyear Wrangler MT/R Kevlar")
+        print("4. Goodyear G32 CARGO")
+        print("5. Goodyear Wrangler SUV")
+        print("6. Goodyear Wrangler Armortrac")
+        print("7. Goodyear Wrangler Adventure")
+        print("8. Goodyear Wrangler Duratrac")
+        print("9. Goodyear G32 PLUS")
+        print("10. Goodyear Wrangler HP All Weather")
+        print("11. Goodyear Wrangler AT/S")
+
+        while True:
+            a=int(input("Escriba el numero de la opcion:"))
+
+            if a==1:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==2:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==3:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==4:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==5:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==6:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==7:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==8:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==9:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==10:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==11:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            else:
+                print("Opcion no valida")
+            if a<=11 and x>=1:
+                break
 
     elif x==3:
-        print("ieieiei")
+        print("Elija modelo de llanta:")
+
+        print("1. Goodyear Assurance")
+
+        while True:
+            a=int(input("Escriba opcion:"))
+
+            if a==1:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            else:
+                print("Opcion no valida")
+            if a==1:
+                break
 
     elif x==4:
-        print("uururr")
+        print("Elija modelo de llanta:")
+
+        print("1. Goodyear Eagle F1 Asymmetric 2 ROF")
+        print("2. Goodyear Eagle F1 Asymmetric 3")
+        print("3. Goodyear Assurance")
+        print("4. Goodyear EfficientGrip Performance ROF")
+        print("5. Goodyear Eagle F1 Asymmetric 2")
+        print("6. Direction Touring")
+        print("7. Goodyear EfficientGrip ROF")
+        print("8. Goodyear Eagle F1 Asymmetric")
+        print("9. Kelly Edge Sport")
+        print("10. Goodyear Excellence ROF")
+        print("11. Goodyear EfficientGrip Performance")
+        print("12. Kelly Edge Touring")
+        print("13. Goodyear EfficientGrip")
+
+        while True:
+            a=int(input("Escriba opcion:"))
+
+            if a==1:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==2:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==3:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==4:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==5:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==6:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==7:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==8:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==9:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==10:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==11:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==12:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==13:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            else:
+                print("Opcion no valida")
+            if a<=13 and x>=1:
+                break
 
     elif x==5:
-        print("icnfjkn")
+        print("Elija modelo de llanta:")
+
+        print("1. Goodyear Eagle F1 Asymmetric 2 ROF")
+        print("2. Goodyear Eagle F1 Asymmetric 3")
+        print("3. Goodyear Assurance")
+        print("4. Goodyear EfficientGrip Performance ROF")
+        print("5. Goodyear Eagle F1 Asymmetric 2")
+        print("6. Direction Touring")
+        print("7. Goodyear EfficientGrip ROF")
+        print("8. Goodyear Eagle F1 Asymmetric")
+        print("9. Kelly Edge Sport")
+        print("10. Goodyear Excellence ROF")
+        print("11. Goodyear EfficientGrip Performance")
+        print("12. Kelly Edge Touring")
+        print("13. Goodyear EfficientGrip")
+
+        while True:
+            a=int(input("Escriba opcion:"))
+
+            if a==1:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==2:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==3:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==4:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==5:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==6:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==7:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==8:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==9:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==10:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==11:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==12:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==13:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            else:
+                print("Opcion no valida")
+            if a<=13 and x>=1:
+                break
 
     elif x==6:
-        print("ndfgjndf")
+        print("Elija modelo de llanta:")
+
+        print("1. Goodyear Eagle F1 Asymmetric 2 ROF")
+        print("2. Goodyear Eagle F1 Asymmetric 3")
+        print("3. Goodyear Assurance")
+        print("4. Goodyear EfficientGrip Performance ROF")
+        print("5. Goodyear Eagle F1 Asymmetric 2")
+        print("6. Direction Touring")
+        print("7. Goodyear EfficientGrip ROF")
+        print("8. Goodyear Eagle F1 Asymmetric")
+        print("9. Kelly Edge Sport")
+        print("10. Goodyear Excellence ROF")
+        print("11. Goodyear EfficientGrip Performance")
+        print("12. Kelly Edge Touring")
+        print("13. Goodyear EfficientGrip")
+
+        while True:
+            a=int(input("Escriba opcion:"))
+
+            if a==1:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==2:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==3:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==4:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==5:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==6:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==7:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==8:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==9:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==10:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==11:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==12:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==13:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            else:
+                print("Opcion no valida")
+            if a<=13 and x>=1:
+                break
     
     elif x==7:
-        print("kndjbjd")
+        print("Elija modelo de llanta:")
+
+        print("1. Goodyear Eagle F1 Asymmetric 2 ROF")
+        print("2. Goodyear Eagle F1 Asymmetric 3")
+        print("3. Goodyear Assurance")
+        print("4. Goodyear EfficientGrip Performance ROF")
+        print("5. Goodyear Eagle F1 Asymmetric 2")
+        print("6. Direction Touring")
+        print("7. Goodyear EfficientGrip ROF")
+        print("8. Goodyear Eagle F1 Asymmetric")
+        print("9. Kelly Edge Sport")
+        print("10. Goodyear Excellence ROF")
+        print("11. Goodyear EfficientGrip Performance")
+        print("12. Kelly Edge Touring")
+        print("13. Goodyear EfficientGrip")
+
+        while True:
+            a=int(input("Escriba opcion:"))
+
+            if a==1:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==2:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==3:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==4:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==5:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==6:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==7:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==8:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==9:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==10:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==11:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==12:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==13:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            else:
+                print("Opcion no valida")
+            if a<=13 and x>=1:
+                break
 
     elif x==8:
-        print("jdgkhdnf")
+        print("Elija modelo de llanta:")
+
+        print("1. Goodyear Eagle F1 Asymmetric 2 ROF")
+        print("2. Goodyear Eagle F1 Asymmetric 3")
+        print("3. Goodyear Assurance")
+        print("4. Goodyear EfficientGrip Performance ROF")
+        print("5. Goodyear Eagle F1 Asymmetric 2")
+        print("6. Direction Touring")
+        print("7. Goodyear EfficientGrip ROF")
+        print("8. Goodyear Eagle F1 Asymmetric")
+        print("9. Kelly Edge Sport")
+        print("10. Goodyear Excellence ROF")
+        print("11. Goodyear EfficientGrip Performance")
+        print("12. Kelly Edge Touring")
+        print("13. Goodyear EfficientGrip")
+
+        while True:
+            a=int(input("Escriba opcion:"))
+
+            if a==1:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==2:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==3:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==4:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==5:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==6:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==7:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==8:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==9:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==10:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==11:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==12:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            elif a==13:
+                print("Precio, pagina, clase de terrono, valoracion, comentarios")
+            else:
+                print("Opcion no valida")
+            if a<=13 and x>=1:
+                break
 
     else:
         print("Opcion invalida")
