912c1cab3c5a97ec6b9ab088c895b261685581a2
fili eduardo <eduardo.asfe276@gmail.com>
2019-11-04 08:45:17 -0500

    BUSQUEDA POR VEHÍCULO

---
+++ base 2.py
@@ -0,0 +1,35 @@
+print("opciones")
+print("1. VEHÍCULO")
+print("2. MEDIDAD DE LLANTAS")
+
+while True:
+    n=int(input("¿Cómo le gustaría hacer la busqueda?"))
+
+    if n==1:
+        a=int(input("Ingrese año del vehículo"))
+
+        if a==2018:
+            print("1. AICHI SAN")
+            print("2. ALFA ROMEO")
+            print("3. ASTON MARTIN")
+            print("4. AUDI")
+            print("5. BAIC")
+            print("6. BAJAJ")
+            m=int(input("Ingrese la marca de su vehículo"))
+            if m==1:
+                print("1. HIACE")
+                n=int(input("Elija el modelo de su vehículo"))
+                if n==1:
+                    print("VERSIÓN/OPCIÓN")
+                    print("1. HIACE 1.3 JUPTIER")
+                    print("2. HIACE 2.8 TDI VAN")
+               
+
+
+
+
+            
+           
+            
+            
+       
