8cfcead7a260cfa8cee6bf7ef16a909c83f0a5f5
Pierre Carhuaricra Tello <pierretello25@gmail.com>
2019-11-11 10:06:10 -0500

    hoja de prueba

--- practica.py
+++ practica.py
@@ -1,191 +1,182 @@
 #Siguiente
 
-elif x== and y== and z==
-    print("Llanta: Goodyear Assurance")
+elif x==205 and y==55 and z==16:
+    print("Neumatico: Goodyear Assurance")
     print(T)
-    s=["Precio: ","Assurance", "pagina de compra:"]
+    s=["Precio aproximado: ","Assurance", "pagina de compra: https://www.goodyear.com.pe/llantas "]
     print(s)
-    print("")
+    print("Grandes surcos laterales en la banda de rodamiento que ayudan a evacuar el agua para un excelente agarre en mojado. 16% mayor vida de la banda de rodamiento debido a un diseño optimizado de la llanta.")
 
 #Siguiente
 
-elif x==255 and y==4 and z==19:
-    print("Llanta: Goodyear Eagle F1 Asymmetric 2 ROF")
+elif x==255 and y==44 and z==19:
+    print("Neumatico: Goodyear Eagle F1 Asymmetric 2 ROF")
     print(T)
-    s=["Precio: ","Eagle", "pagina de compra:"]
+    s=["Precio aproximado: ","Eagle", "pagina de compra: https://www.goodyear.com.pe/llantas"]
     print(s)
-    print("")
+    print("Menor consumo de combustible La mayor eficiencia del consumo de combustible le ayuda a reducir gastos y a minimizar las emisiones CO2, lo que es bueno para su bolsillo y para el medio ambiente.")
 elif x==225 and y==45 and z==17:
-    print("Llanta: Goodyear Eagle F1 Asymmetric 3")
+    print("Neumatico: Goodyear Eagle F1 Asymmetric 3")
     print(T)
-    s=["Precio: ","Eagle", "pagina de compra:"]
+    s=["Precio aproximado: ","Eagle", "pagina de compra: https://www.goodyear.com.pe/llantas"]
     print(s)
-    print("")
-elif x==255 and y==4 and z==19:
-    print("Llanta: Goodyear Eagle F1 Asymmetric 2")
+    print("La tecnología ActiveBraking proporciona mayor contacto de la llanta con el suelo en el frenado. Estructura reforzada que garantiza mayor precisión en la dirección y durabilidad.")
+elif x==255 and y==40 and z==19:
+    print("Neumatico: Goodyear Eagle F1 Asymmetric 2")
     print(T)
-    s=["Precio: ","Eagle", "pagina de compra:"]
+    s=["Precio aproximado: ","Eagle", "pagina de compra: https://www.goodyear.com.pe/llantas"]
+    print(s)
+    print("La tecnología ActiveBraking proporciona mayor contacto de la llanta con el suelo en el frenado.")
+elif x==225 and y==40 and z==18:
+    print("Neumatico: Goodyear Eagle F1 Asymmetric")
+    print(T)
+    s=["Precio aproximado: ","Eagle", "pagina de compra: https://www.goodyear.com.pe/llantas"]
+    print(s)
+    print("Conducción de alto rendimiento. Obtenga un mayor rendimiento de su coche de alta potencia. El compuesto de sílice de la superficie mejora el agarre para una maniobrabilidad fiable sobre calzadas secas y mojadas.")
+elif x==245 and y==45 and z==19:
+    print("Neumatico: Goodyear Excellence ROF")
+    print(T)
+    s=["Precio aproximado aproximado: ","Eagle", "pagina de compra: https://www.goodyear.com.pe/llantas"]
+    print(s)
+    print("Creada con compuesto especial de polimero, Excellence brinda una excepcional adherencia y agarre en curvas, con menor distancia de frenado.")
+
+#Siguiente
+
+elif x==255 and y==70 and z==18:
+    print("Neumatico: Goodyear Wrangler All-Terrain Adventure")
+    print(T)
+    s=["Precio aproximado aproximado: ","Wrangler", "pagina de compra: https://www.goodyear.com.pe/llantas"]
+    print(s)
+    print("37 por ciento superior en tracción en piso mojado. Agilidad en terrenos difíciles (barro, arena y piedra).")
+elif x==265 and y==85 and z==16:
+    print("Neumatico: Goodyear Wrangler MT/R Kevlar")
+    print(T)
+    s=["Precio aproximado aproximado: ","Wrangler", "pagina de compra: https://www.goodyear.com.pe/llantas"]
+    print(s)
+    print("Insuperable agarre en caminos Off-Road y lluvia. Compuesto de sílica en la banda de rodamiento que proporciona mayor tracción. Extraordinaria tracción en lodo, grava y caminos rocosos. Costado especial de la llanta con un agresivo dibujo que brinda tracción lateral.")
+elif x==215 and y==70 and z==16:
+    print("Neumatico: Goodyear Wrangler SUV")
+    print(T)
+    s=["Precio aproximado aproximado: ","Wrangler", "pagina de compra: https://www.goodyear.com.pe/llantas"]
+    print(s)
+    print("Mejor adherencia en piso mojado. Kilometraje y ahorro de combustible.")
+elif x==245 and y==70 and z==16:
+    print("Neumatico: Goodyear Wrangler Armortrac")
+    print(T)
+    s=["Precio aproximado aproximado: ","Wrangler", "pagina de compra: https://www.goodyear.com.pe/llantas"]
+    print(s)
+    print("Mejor desempeño en piso mojado. Kilometraje y resistencia en condiciones de carretera y todo terreno.")
+elif x==235 and y==70 and z==16:
+    print("Neumatico: Goodyear Wrangler Adventure")
+    print(T)
+    s=["Precio aproximado aproximado: ","Wrangler", "pagina de compra: https://www.goodyear.com.pe/llantas"]
+    print(s)
+    print("Construcción reforzada en el costado con Durawal TechnologyTM  que proporciona  mayor resistencia a cortes y perforaciones. Láminas en tacos y hombros que proporcionan mayor capacidad de enfriamiento y superior alcance en kilometroje.")
+elif x==275 and y==55 and z==18:
+    print("Neumatico: Goodyear Wrangler Duratrac")
+    print(T)
+    s=["Precio aproximado aproximado: ","Wrangler", "pagina de compra: https://www.goodyear.com.pe/llantas"]
+    print(s)
+    print("Rodar silencioso: Bloques centrales altamente angulados que aumentan la tracción y la estabilidad lateral y reducen el ruido externo.")
+elif x==275 and y==70 and z==16:
+    print("Neumatico: Goodyear Wrangler HP All Weather")
+    print(T)
+    s=["Precio aproximado aproximado: ","Wrangler", "pagina de compra: https://www.goodyear.com.pe/llantas"]
+    print(s)
+    print("Rodar suave y confortable. Mayor poder de tracción en pisos secos o mojados.")
+elif x==205 and y==80 and z==16:
+    print("Neumatico: Goodyear Wrangler AT/S")
+    print(T)
+    s=["Precio aproximado aproximado: ","Wrangler", "pagina de compra: https://www.goodyear.com.pe/llantas"]
+    print(s)
+    print("Excelente tracción en todo tipo de terreno gracias a sus canales autolimpiantes que permiten una mejor evacuación del agua y barro. Su diseño profundo en la parte exterior de los hombros y amplios bloques laterales ofrecen una adecuada protección para el costado.")
+
+#Siguiente
+
+elif x== and y== and z==
+    print("Neumatico: Goodyear EfficientGrip Performance SUV")
+    print(T)
+    s=["Precio aproximado aproximado: ","Efficient", "pagina de compra: https://www.goodyear.com.pe/llantas"]
     print(s)
     print("")
 elif x== and y== and z==
-    print("Llanta: Goodyear Eagle F1 Asymmetric")
+    print("Neumatico: Goodyear EfficientGrip Performance")
     print(T)
-    s=["Precio: ","Eagle", "pagina de compra:"]
+    s=["Precio aproximado aproximado: ","Efficient", "pagina de compra: https://www.goodyear.com.pe/llantas"]
     print(s)
     print("")
 elif x== and y== and z==
-    print("Llanta: Goodyear Excellence ROF")
+    print("Neumatico: Goodyear EfficientGrip")
     print(T)
-    s=["Precio: ","Eagle", "pagina de compra:"]
+    s=["Precio aproximado aproximado: ","Efficient", "pagina de compra: https://www.goodyear.com.pe/llantas"]
     print(s)
     print("")
 
 #Siguiente
 
 elif x== and y== and z==
-    print("Llanta: Goodyear Wrangler All-Terrain Adventure")
+    print("Neumatico: Goodyear Eagle F1 Asymmetric 3")
     print(T)
-    s=["Precio: ","Wrangler", "pagina de compra:"]
+    s=["Precio aproximado aproximado: ","Alto desempeño", "pagina de compra: https://www.goodyear.com.pe/llantas"]
     print(s)
     print("")
 elif x== and y== and z==
-    print("Llanta: Goodyear Wrangler MT/R Kevlar")
+    print("Neumatico: Goodyear Eagle F1 Asymmetric 2")
     print(T)
-    s=["Precio: ","Wrangler", "pagina de compra:"]
+    s=["Precio aproximado aproximado: ","Alto desempeño", "pagina de compra: https://www.goodyear.com.pe/llantas"]
     print(s)
     print("")
 elif x== and y== and z==
-    print("Llanta: Goodyear Wrangler SUV")
+    print("Neumatico: Goodyear Eagle F1 Asymmetric")
     print(T)
-    s=["Precio: ","Wrangler", "pagina de compra:"]
+    s=["Precio aproximado aproximado: ","Alto desempeño", "pagina de compra: https://www.goodyear.com.pe/llantas"]
     print(s)
     print("")
 elif x== and y== and z==
-    print("Llanta: Goodyear Wrangler Armortrac")
+    print("Neumatico: Kelly Edge Sport")
     print(T)
-    s=["Precio: ","Wrangler", "pagina de compra:"]
+    s=["Precio aproximado aproximado: ","Alto desempeño", "pagina de compra: https://www.goodyear.com.pe/llantas"]
     print(s)
     print("")
 elif x== and y== and z==
-    print("Llanta: Goodyear Wrangler Adventure")
+    print("Neumatico: Goodyear EfficientGrip Performance")
     print(T)
-    s=["Precio: ","Wrangler", "pagina de compra:"]
+    s=["Precio aproximado aproximado: ","Alto desempeño", "pagina de compra: https://www.goodyear.com.pe/llantas"]
     print(s)
     print("")
 elif x== and y== and z==
-    print("Llanta: Goodyear Wrangler Duratrac")
+    print("Neumatico: Goodyear EfficientGrip")
     print(T)
-    s=["Precio: ","Wrangler", "pagina de compra:"]
-    print(s)
-    print("")
-elif x== and y== and z==
-    print("Llanta: Goodyear Wrangler HP All Weather")
-    print(T)
-    s=["Precio: ","Wrangler", "pagina de compra:"]
-    print(s)
-    print("")
-elif x== and y== and z==
-    print("Llanta: Goodyear Wrangler AT/S")
-    print(T)
-    s=["Precio: ","Wrangler", "pagina de compra:"]
+    s=["Precio aproximado aproximado: ","Alto desempeño", "pagina de compra: https://www.goodyear.com.pe/llantas"]
     print(s)
     print("")
 
 #Siguiente
 
 elif x== and y== and z==
-    print("Llanta: Goodyear EfficientGrip Performance SUV")
+    print("Neumatico: Goodyear Eagle F1 Asymmetric 2 ROF")
     print(T)
-    s=["Precio: ","Efficient", "pagina de compra:"]
+    s=["Precio aproximado aproximado: ","Run on Flat", "pagina de compra: https://www.goodyear.com.pe/llantas"]
     print(s)
     print("")
 elif x== and y== and z==
-    print("Llanta: Goodyear EfficientGrip Performance")
+    print("Neumatico: Goodyear EfficientGrip Performance ROF")
     print(T)
-    s=["Precio: ","Efficient", "pagina de compra:"]
+    s=["Precio aproximado aproximado: ","Run on Flat", "pagina de compra: https://www.goodyear.com.pe/llantas"]
     print(s)
     print("")
 elif x== and y== and z==
-    print("Llanta: Goodyear EfficientGrip")
+    print("Neumatico: Goodyear EfficientGrip ROF")
     print(T)
-    s=["Precio: ","Efficient", "pagina de compra:"]
-    print(s)
-    print("")
-
-#Siguiente
-
-elif x== and y== and z==
-    print("Llanta: Goodyear Eagle F1 Asymmetric 3")
-    print(T)
-    s=["Precio: ","Alto desempeño", "pagina de compra:"]
+    s=["Precio aproximado aproximado: ","Run on Flat", "pagina de compra: https://www.goodyear.com.pe/llantas"]
     print(s)
     print("")
 elif x== and y== and z==
-    print("Llanta: Goodyear Eagle F1 Asymmetric 2")
+    print("Neumatico: Goodyear Excellence ROF")
     print(T)
-    s=["Precio: ","Alto desempeño", "pagina de compra:"]
+    s=["Precio aproximado: ","Run on Flat", "pagina de compra: https://www.goodyear.com.pe/llantas"]
     print(s)
     print("")
-elif x== and y== and z==
-    print("Llanta: Goodyear Eagle F1 Asymmetric")
-    print(T)
-    s=["Precio: ","Alto desempeño", "pagina de compra:"]
-    print(s)
-    print("")
-elif x== and y== and z==
-    print("Llanta: Kelly Edge Sport")
-    print(T)
-    s=["Precio: ","Alto desempeño", "pagina de compra:"]
-    print(s)
-    print("")
-elif x== and y== and z==
-    print("Llanta: Goodyear EfficientGrip Performance")
-    print(T)
-    s=["Precio: ","Alto desempeño", "pagina de compra:"]
-    print(s)
-    print("")
-elif x== and y== and z==
-    print("Llanta: Goodyear EfficientGrip")
-    print(T)
-    s=["Precio: ","Alto desempeño", "pagina de compra:"]
-    print(s)
-    print("")
-
-#Siguiente
-
-elif x== and y== and z==
-    print("Llanta: Goodyear Eagle F1 Asymmetric 2 ROF")
-    print(T)
-    s=["Precio: ","Run on Flat", "pagina de compra:"]
-    print(s)
-    print("")
-elif x== and y== and z==
-    print("Llanta: Goodyear EfficientGrip Performance ROF")
-    print(T)
-    s=["Precio: ","Run on Flat", "pagina de compra:"]
-    print(s)
-    print("")
-elif x== and y== and z==
-    print("Llanta: Goodyear EfficientGrip ROF")
-    print(T)
-    s=["Precio: ","Run on Flat", "pagina de compra:"]
-    print(s)
-    print("")
-elif x== and y== and z==
-    print("Llanta: Goodyear Excellence ROF")
-    print(T)
-    s=["Precio: ","Run on Flat", "pagina de compra:"]
-    print(s)
-    print("")
-
-#Siguiente
-
-else:
-    print("Llanta actualmente no existente o datos no correctos")
-    print("Ingrese datos nuevamente")
-    x=int(input("Ingrese el ancho"))
-    y=int(input("Ingrese perfil"))
-    z=int(input("Ingrese el radio"))
 
 
     
