a82b4002b73657f1bf0c845d0c5c325a0dcb33b7
Pierre Carhuaricra Tello <pierretello25@gmail.com>
2019-11-11 08:59:11 -0500

    reemplazante

--- practica.py
+++ practica.py
@@ -3,152 +3,153 @@ y=int(input("Ingrese perfil"))
 z=int(input("Ingrese el radio"))
 
 if x==255 and y==4 and z==19:
-    print("Llanta: Goodyear Eagle F1 Asymmetric 2 ROF")
+    print("Neumático: Goodyear Eagle F1 Asymmetric 2 ROF")
     print(x,"/",y,"R",z)
     s=["Precio: ","ideal para ciudad", "pagina de compra:"]
     print(s)
     print("Control preciso y potente Aproveche al máximo su coche con un neumático fabricado para maximizar el rendimiento de alta potencia. Un compuesto de sílice especial ofrece un agarre fiable en carreteras secas y mojadas con una carcasa rígida que aumenta la precisión de la dirección incluso a alta velocidad.")
 elif x==225 and y==45 and z==17:
-    print("Llanta: Goodyear Eagle F1 Asymmetric 3")
+    print("Neumático: Goodyear Eagle F1 Asymmetric 3")
     print(T)
     s=["Precio: ","ideal para ciudad", "pagina de compra:"]
     print(s)
-    print("")
-elif x== and y== and z==
-    print("Llanta: Goodyear Assurance")
+    print("Neumático para camiones ligeros que puede convertirse en su socio más fiable proporcionándole 10.000 km adicionales, ahorrándole combustible y ofreciéndole distancias de frenado más cortas sobre carreteras mojadas.")
+    print("Reseña: Comportamiento cómodo, tranquilo y predecible. Quizás no para todos los paseos deportivos debido al perfil ligeramente más alto, sino para las carreras normales, el mejor.")
+elif x==205 and y==55 and z==16:
+    print("Neumático: Goodyear Assurance")
     print(T)
     s=["Precio: ","ideal para ciudad", "pagina de compra:"]
     print(s)
-    print("")
-elif x== and y== and z==
+    print("2,600 millas/4,000 kilómetros en base a una mejora de ahorro de combustible del 4%.")
+elif x==225 and y==50 and z==17:
     print("Llanta: Goodyear EfficientGrip Performance ROF")
     print(T)
     s=["Precio: ","ideal para ciudad", "pagina de compra:"]
     print(s)
-    print("")
+    print("Distancia de frenado reducida Siéntase más seguro al volante en cada viaje con la tecnología ActiveBraking. Esta característica aumenta la superficie de contacto entre el neumático y la carretera, para reducir drásticamente las distancias de frenado, hasta un 8% más cortas en carreteras mojadas y hasta un 3 por ciento en carreteras secas.")
 elif x==255 and y==4 and z==19:
     print("Llanta: Goodyear Eagle F1 Asymmetric 2")
     print(T)
     s=["Precio: ","ideal para ciudad", "pagina de compra:"]
     print(s)
-    print("")
-elif x== and y== and z==
+    print("Control preciso y potente Aproveche al máximo su coche con un neumático fabricado para maximizar el rendimiento de alta potencia. Un compuesto de sílice especial ofrece un agarre fiable en carreteras secas y mojadas con una carcasa rígida que aumenta la precisión de la dirección incluso a alta velocidad.")
+elif x==175 and y==70 and z==13
     print("Llanta: Direction Touring")
     print(T)
     s=["Precio: ","ideal para ciudad", "pagina de compra:"]
     print(s)
-    print("")
-elif x== and y== and z==
+    print("Surcos transversales y longitudinales más anchos, lo que brinda mejor adherencia en piso mojado.")
+elif x==235 and y==45 and z==19:
     print("Llanta: Goodyear EfficientGrip ROF")
     print(T)
     s=["Precio: ","ideal para ciudad", "pagina de compra:"]
     print(s)
-    print("")
-elif x== and y== and z==
+    print("Para que un pinchazo nunca vuelva a detener su viaje: gracias a la revolucionaria tecnología RunOnFlat, podrá conducir una distancia de hasta 80 km después de un pinchazo o reventón.")
+elif x==225 and y==40 and z==18:
     print("Llanta: Goodyear Eagle F1 Asymmetric")
     print(T)
     s=["Precio: ","ideal para ciudad", "pagina de compra:"]
     print(s)
-    print("")
-elif x== and y== and z==
+    print("Reducción de la distancia de frenado en carreteras mojadas y secas. Deténgase con total confianza cuando sea cuestión de milésimas de segundo gracias a la tecnología ActiveBraking, que aumenta el contacto del neumático con la calzada cuando frena.")
+elif x==185 and y==60 and z==14:
     print("Llanta: Kelly Edge Sport")
     print(T)
     s=["Precio: ","ideal para ciudad", "pagina de compra:"]
     print(s)
-    print("")
-elif x== and y== and z==
+    print("Bajo índice de desgaste de la banda de rodamiento: Ventaja de hasta 6 por ciento si se compara a su antecesor, entregando un alto kilometraje.")
+elif x==225 and y==55 and z==17:
     print("Llanta: Goodyear Excellence ROF")
     print(T)
     s=["Precio: ","ideal para ciudad", "pagina de compra:"]
     print(s)
-    print("")
-elif x== and y== and z==
+    print("Las plantas automotrices más reconocidas del mercado equipan sus vehículos con llantas que tienen tecnología RunOnFlat®. Esta tecnología permite que la llanta ruede sin presión del aire hasta 80 km/h.")
+elif x==205 and y==60 and z==16:
     print("Llanta: Goodyear EfficientGrip Performance")
     print(T)
     s=["Precio: ","ideal para ciudad", "pagina de compra:"]
     print(s)
-    print("")
-elif x== and y== and z==
+    print("El mejor frenado sobre terreno mojado con una resistencia mínima La tecnología WearControl de Goodyear está diseñada para encontrar el equilibrio ideal entre un agarre firme sobre carreteras mojadas y una baja resistencia a la rodadura, para reducir el consumo de combustible y garantizar una conducción más estable.")
+elif x==175 and y==65 and z==14:
     print("Llanta: Kelly Edge Touring")
     print(T)
     s=["Precio: ","ideal para ciudad", "pagina de compra:"]
     print(s)
-    print("")
-elif x== and y== and z==
+    print("Kelly Edge Touring es una llanta desarrollada para autos de baja a media potencia. Su desempeño en pistas mojadas es de hasta un 20 por ciento superior, en comparación con su predecesor.")
+elif x==205 and y==55 and z==16:
     print("Llanta: Goodyear EfficientGrip")
     print(T)
     s=["Precio: ","ideal para ciudad", "pagina de compra:"]
     print(s)
-    print("")
+    print("Con estructura liviana y compuesto avanzado de banda, EfficientGrip necesita menos energía para rodar. Disfruta viajes más silenciosos gracias al diseño de los bloques que reduce los niveles de ruido.")
 
 #Siguiente
 
-elif x== and y== and z==
+elif x==255 and y==60 and z==17:
     print("Llanta: Goodyear EfficientGrip Performance SUV")
     print(T)
     s=["Precio: ","Camioneta", "pagina de compra:"]
     print(s)
-    print("")
-elif x== and y== and z==
+    print("Desarrollado con la tecnología Fuel Saving, que proporciona ahorro de combustible. Ofrece un desempeño hasta 30 % mejor en maniobrabilidad y 15 por ciento superior en tracción y frenado en piso mojado.")
+elif x==255 and y==70 and z==18:
     print("Llanta: Goodyear Wrangler All-Terrain Adventure")
     print(T)
     s=["Precio: ","Camioneta", "pagina de compra:"]
     print(s)
-    print("")
-elif x== and y== and z==
+    print("El neumático versátil todoterreno con la rigidez de Kevlar® para circular fuera de carretera en cualquier momento. Capa reforzada de Kevlar®. Resiste los pinchazos y los cortes para una conducción segura fuera de la carretera.")
+elif x==285 and y==75 and z==16:
     print("Llanta: Goodyear Wrangler MT/R Kevlar")
     print(T)
     s=["Precio: ","Camioneta", "pagina de compra:"]
     print(s)
-    print("")
-elif x== and y== and z==
+    print("Insuperable agarre en caminos Off-Road y lluvia. Compuesto de sílica en la banda de rodamiento que proporciona mayor tracción. Extraordinaria tracción en lodo, grava y caminos rocosos. Costado especial de la llanta con un agresivo dibujo que brinda tracción lateral.")
+elif x==225 and y==65 and z==16:
     print("Llanta: Goodyear G32 CARGO")
     print(T)
     s=["Precio: ","Camioneta", "pagina de compra:"]
     print(s)
-    print("")
-elif x== and y== and z==
+    print("Rodar tranquilo, confortable y silencioso. Excelente kilometraje y tracción. Fuerte resistencia a las condiciones rigurosas de los servicios comerciales.")
+elif x==235 and y==65 and z==17:
     print("Llanta: Goodyear Wrangler SUV")
     print(T)
     s=["Precio: ","Camioneta", "pagina de compra:"]
     print(s)
-    print("")
-elif x== and y== and z==
+    print("Mejor adherencia en piso mojado. Kilometraje y ahorro de combustible.")
+elif x==245 and y==75 and z==16:
     print("Llanta: Goodyear Wrangler Armortrac")
     print(T)
     s=["Precio: ","Camioneta", "pagina de compra:"]
     print(s)
-    print("")
-elif x== and y== and z==
+    print("Mejor desempeño en piso mojado. Kilometraje y resistencia en condiciones de carretera y todo terreno.")
+elif x==255 and y==70 and z==18:
     print("Llanta: Goodyear Wrangler Adventure")
     print(T)
     s=["Precio: ","Camioneta", "pagina de compra:"]
     print(s)
-    print("")
-elif x== and y== and z==
+    print("Láminas en tacos y hombros que proporcionan mayor capacidad de enfriamiento y superior alcance en kilometroje. Surcos longitudinales y transversales para mejor desalojo de agua y limpieza de barro y piedras, permitiendo un mejor desempeño en todo tipo de terreno.")
+elif x==265 and y==65 and z==17:
     print("Llanta: Goodyear Wrangler Duratrac")
     print(T)
     s=["Precio: ","Camioneta", "pagina de compra:"]
     print(s)
-    print("")
-elif x== and y== and z==
+    print("Mayor tracción: gracias a la tecnología TractiveGroove™, que consiste en pequeños dientes de sierra en la base de los surcos para mejorar la tracción en lodo profundo. Rodar silencioso: Bloques centrales altamente angulados que aumentan la tracción y la estabilidad lateral y reducen el ruido externo.")
+elif x==215 and y==70 and z==14:
     print("Llanta: Goodyear G32 PLUS")
     print(T)
     s=["Precio: ","Camioneta", "pagina de compra:"]
     print(s)
-    print("")
-elif x== and y== and z==
+    print("Mejor rendimiento en curvas y frenado. Resistencia a impactos y temperatura elevadas.")
+elif x==235 and y==60 and z==18:
     print("Llanta: Goodyear Wrangler HP All Weather")
     print(T)
     s=["Precio: ","Camioneta", "pagina de compra:"]
     print(s)
-    print("")
-elif x== and y== and z==
+    print("Las acanaladuras de la banda de rodadura aseguran la rápida dispersión del agua, lo que ayuda a mantener un control óptimo sobre mojado.")
+elif x==205 and y==40 and z==16:
     print("Llanta: Goodyear Wrangler AT/S")
     print(T)
     s=["Precio: ","Camioneta", "pagina de compra:"]
     print(s)
-    print("")
+    print("Su diseño profundo en la parte exterior de los hombros y amplios bloques laterales ofrecen una adecuada protección para el costado. Diseño de la banda de rodamiento optimizado para brindar un rodar suave y proveer un desgaste parejo.")
 
 #Siguiente
 
@@ -329,6 +330,15 @@ elif x== and y== and z==
     s=["Precio: ","Run on Flat", "pagina de compra:"]
     print(s)
     print("")
+
+#Siguiente
+
+else:
+    print("Llanta actualmente no existente o datos no correctos")
+    print("Ingrese datos nuevamente")
+    x=int(input("Ingrese el ancho"))
+    y=int(input("Ingrese perfil"))
+    z=int(input("Ingrese el radio"))
 
 
     
